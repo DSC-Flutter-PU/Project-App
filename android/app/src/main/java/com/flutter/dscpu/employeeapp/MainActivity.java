cc22edc8ee394f75f94cdc2890df126a8e3b38a3
MawiraIke <mawiraike@gmail.com>
2020-02-29 19:22:26 +0300

    Version compatible with android x
    Signed-off-by: Ike Mawira <mawiraike@gmail.com>

--- android/app/src/main/java/com/flutter/dscpu/employeeapp/MainActivity.java
+++ android/app/src/main/java/com/flutter/dscpu/employeeapp/MainActivity.java
@@ -1,6 +1,6 @@
 package com.flutter.dscpu.employeeapp;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
